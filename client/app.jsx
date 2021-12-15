3cc8cc0f8fbb36ed677efd05ce47c1f60c0518cb
Thanh Ha <thanh3203@gmail.com>
2021-12-15 13:24:57 -0800

    Initialized Babel to transpile React

---
+++ client/app.jsx
@@ -0,0 +1,15 @@
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        Hello REACT!
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,3 @@
+import App from './app.js';
+
+ReactDOM.render(<App />, document.getElementById('app'));
